bdbf93892e4ad494931ce7467bb26b4e0049554b
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-01-14 16:19:43 -0500

    inital commit

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args )
+        {
+            string output;
+            if (args.Length == 0) 
+            {
+                output = "Nobody";
+            } else {
+                output = args[0];
+            }
+
+            Console.WriteLine($"Hello, {output}!");
+        }
+    }
+}
